bf5f367bc7ab6106c818d9f73ebdac8899d00f43
Mohamed Mosalm <mohamed.mosalm2002@gmail.com>
2025-07-09 12:42:33 +0300

    feat: refactor task controller to use asyncWrapper and implement global error handling

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,97 +1,74 @@
-import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
-import { sendSuccessResponse, sendErrorResponse } from '../utils/responseHandler';
+import { sendSuccessResponse } from '../utils/responseHandler';
+import asyncWrapper from '../utils/asyncWrapper';
+import { AppError } from '../middlewares/errorHandler';
 
 const prismaClient = new PrismaClient();
 
-const getAllTasks = async (_req: Request, res: Response) => {
-  try {
-    const tasks = await prismaClient.task.findMany();
-    sendSuccessResponse(res, 200, 'Tasks fetched successfully', tasks);
-  } catch (error) {
-    console.error('Error fetching tasks:', error);
-    sendErrorResponse(res, 500, 'Internal Server Error', error instanceof Error ? error.message : 'Unknown Error');
+const getAllTasks = asyncWrapper(async (_req: Request, res: Response) => {
+  const tasks = await prismaClient.task.findMany();
+  sendSuccessResponse(res, 200, 'Tasks fetched successfully', tasks);
+});
+
+const getTaskById = asyncWrapper(async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const task = await prismaClient.task.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!task) {
+    throw new AppError('Task not found', 404);
   }
-};
 
-const getTaskById = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
+  sendSuccessResponse(res, 200, 'Task fetched successfully', task);
+});
 
-    const task = await prismaClient.task.findUnique({
-      where: {
-        id: parseInt(id),
-      },
-    });
-    sendSuccessResponse(res, 200, 'Task fetched successfully', task);
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
-      sendErrorResponse(res, 404, 'Task not found');
-    }
-    console.error('Error fetching tasks:', error);
-    sendErrorResponse(res, 500, 'Internal Server Error', error instanceof Error ? error.message : 'Unknown Error');
-  }
-};
-
-const createTask = async (req: Request, res: Response) => {
+const createTask = asyncWrapper(async (req: Request, res: Response) => {
   const { title, content } = req.body;
 
-  try {
-    const newTask = await prismaClient.task.create({
-      data: {
-        title,
-        content,
-      },
-    });
-    sendSuccessResponse(res, 201, 'Task created successfully', newTask);
-  } catch (error) {
-    console.error('Error creating task:', error);
-    sendErrorResponse(res, 500, 'Internal Server Error', error instanceof Error ? error.message : 'Unknown Error');
+  if (!title || !content) {
+    throw new AppError('Title and content are required', 400);
   }
-};
 
-const updateTask = async (req: Request, res: Response) => {
+  const newTask = await prismaClient.task.create({
+    data: {
+      title,
+      content,
+    },
+  });
+  sendSuccessResponse(res, 201, 'Task created successfully', newTask);
+});
+
+const updateTask = asyncWrapper(async (req: Request, res: Response) => {
   const { title, content, completed } = req.body;
   const { id } = req.params;
 
-  try {
-    const updatedTask = await prismaClient.task.update({
-      where: {
-        id: parseInt(id),
-      },
-      data: {
-        title,
-        content,
-        completed,
-      },
-    });
-    sendSuccessResponse(res, 200, 'Task updated successfully', updatedTask);
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
-      sendErrorResponse(res, 404, 'Task not found');
-    }
-    console.error('Error updating task:', error);
-    sendErrorResponse(res, 500, 'Internal Server Error', error instanceof Error ? error.message : 'Unknown Error');
-  }
-};
+  const updatedTask = await prismaClient.task.update({
+    where: {
+      id: parseInt(id),
+    },
+    data: {
+      title,
+      content,
+      completed,
+    },
+  });
+  sendSuccessResponse(res, 200, 'Task updated successfully', updatedTask);
+});
 
-const deleteTask = async (req: Request, res: Response) => {
+const deleteTask = asyncWrapper(async (req: Request, res: Response) => {
   const { id } = req.params;
 
-  try {
-    await prismaClient.task.delete({
-      where: {
-        id: parseInt(id),
-      },
-    });
-    sendSuccessResponse(res, 204, 'Task Deleted Successfully');
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
-      sendErrorResponse(res, 404, 'Task not found');
-    }
-    console.error('Error deleteing task:', error);
-    sendErrorResponse(res, 500, 'Internal Server Error', error instanceof Error ? error.message : 'Unknown Error');
-  }
-};
+  await prismaClient.task.delete({
+    where: {
+      id: parseInt(id),
+    },
+  });
+  sendSuccessResponse(res, 204, 'Task Deleted Successfully');
+});
 
 export { getAllTasks, createTask, getTaskById, updateTask, deleteTask };

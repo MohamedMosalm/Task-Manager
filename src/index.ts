854de35b5f63f90b86c09911e2ea023e18719f38
Mohamed Mosalm <mohamed.mosalm2002@gmail.com>
2025-07-09 12:02:07 +0300

    feat: add 404 error handling for undefined routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import taskRouter from './routes/tasksRoutes';
+import { sendErrorResponse } from './utils/responseHandler';
 
 const app = express();
 
@@ -8,6 +9,10 @@ app.use(express.json());
 const apiPrefix = process.env.API_PREFIX || '/api/v1';
 
 app.use(`${apiPrefix}/tasks`, taskRouter);
+
+app.all('*', (req, res) => {
+  sendErrorResponse(res, 404, `Can't find ${req.originalUrl} on this server!`);
+});
 
 const PORT = process.env.SERVER_PORT || 3000;
 
